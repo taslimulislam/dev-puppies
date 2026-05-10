a5c21da02a22fe109cbb30706f1936a5591fdd41
Taslimul Islam <taslimul4001@gmail.com>
2026-05-10 23:28:43 +0600

    fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { Container } from './components/Container'
 import { Header } from './components/Header'
 import { NewPuppyForm } from './components/NewPuppyForm'
@@ -28,6 +28,7 @@ function Main() {
   const [puppies, setPuppies] = useState<Puppy[]>(puppiesData) 
   return (
     <main>
+      <ApiPuppies/>
       <div className="mt-24 grid gap-8 sm:grid-cols-2">
         <Search searchQuery={searchQuery} setSearchQuery={setSearchQuery}/>
         <Shortlist puppies={puppies}  liked={liked} setLiked={setLiked}/>
@@ -35,5 +36,38 @@ function Main() {
       <PuppiesList searchQuery={searchQuery} puppies={puppies} liked={liked} setLiked={setLiked}/>
       <NewPuppyForm puppies={puppies} setPuppies={setPuppies}/>
     </main>
+  )
+}
+
+function ApiPuppies() {
+  const [apiPuppies, setApiPuppies] = useState<[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>("");
+  useEffect(
+    () => {
+        async function getPuppies() {
+          setIsLoading(true);
+          try {
+            const response = await fetch("http://react-from-scratch-api.test/api/puppies");
+
+            if (!response.ok) {
+              const errorData = await response.json();
+              setError(`${errorData.message} : ${errorData.details}`);
+              throw errorData;
+            }
+
+            const data = await response.json();
+            setApiPuppies(data);
+            
+          } catch (error) {
+            console.error(error);
+          }
+          setIsLoading(false);
+        }
+        getPuppies();
+    },
+    [
+     //re-run the effect 
+    ]
   )
 }
